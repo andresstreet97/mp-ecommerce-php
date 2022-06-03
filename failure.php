758883e6277359b430297be12f390b309b0252e7
Jose Andres <106769060+andresstreet97@users.noreply.github.com>
2022-06-03 11:57:40 -0300

    Create failure.php

---
+++ failure.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <header>
+    </header>
+    <body>
+      <h1>Pago Rechazado</h1>
+      <?php
+        echo("
+          <p>Pago rechazado</p>
+        ");
+      ?>
+    </body>
+    <footer>
+    </footer>
+  </body>
+</html>
