0081c9abfa672dfd1afb8340c84192813b602a29
Jose Andres <106769060+andresstreet97@users.noreply.github.com>
2022-06-03 13:40:52 -0300

    Update success.php

--- success.php
+++ success.php
@@ -11,9 +11,9 @@
       <?php
         echo("
           <p>Payment Id : ".$_GET['payment_id']."</p></br>
-          <p>Payment Id : ".$_GET['status']."</p></br>
-          <p>Payment Id : ".$_GET['external_reference']."</p></br>
-          <p>Payment Id : ".$_GET['merchant_id']."</p></br>
+          <p>Status : ".$_GET['status']."</p></br>
+          <p>External reference : ".$_GET['external_reference']."</p></br>
+          <p>Merchant Id : ".$_GET['merchant_order_id']."</p></br>
         ");
       ?>
     </body>
