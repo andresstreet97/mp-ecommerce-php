730c423752165b0277711f43475278db2410269f
Jose Andres <106769060+andresstreet97@users.noreply.github.com>
2022-06-03 11:59:36 -0300

    Create pending.php

---
+++ pending.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <header>
+    </header>
+    <body>
+      <h1>Pago pendiente</h1>
+      <?php
+        echo("
+          <p>Pago pendiente</p>
+        ");
+      ?>
+    </body>
+    <footer>
+    </footer>
+  </body>
+</html>
